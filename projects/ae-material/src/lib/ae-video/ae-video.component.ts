8f06a47f4e6a9d55f166ffeed761b148fa0885cf
Ahmet Emrebas <aemrebasus@gmail.com>
2020-10-14 00:41:29 -0500

    Video Component Controllers and Volume

--- projects/ae-material/src/lib/ae-video/ae-video.component.ts
+++ projects/ae-material/src/lib/ae-video/ae-video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AeToolbar } from '../ae-toolbar/ae-toolbar.component';
 
 @Component({
@@ -6,30 +6,147 @@ import { AeToolbar } from '../ae-toolbar/ae-toolbar.component';
   templateUrl: './ae-video.component.html',
   styleUrls: ['./ae-video.component.css']
 })
-export class AeVideoComponent implements OnInit {
+export class AeVideoComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  private documentElement = document.documentElement;
+  public isFullScreen = false;
+  public progressBarValue = 0;
+  public volumeValue = 40;
+  public isVolumeHidden = true;
+
+
+  @ViewChild('videoElement') videoElement: ElementRef<HTMLVideoElement>;
 
   /**
    * @description player controller
    */
   public controls: AeToolbar = {
     list: [
-      { action: () => console.log('play'), icon: 'play_arrow', location: 'left' },
-      { action: () => console.log('stop'), icon: 'stop', location: 'left' },
-      { action: () => console.log('pause'), icon: 'pause', location: 'left' },
-      { action: () => console.log('previous'), icon: 'skip_previous', location: 'left' },
-      { action: () => console.log('next'), icon: 'skip_next', location: 'left' },
-      { action: () => console.log('volume up'), icon: 'volume_up', location: 'left' },
-      { action: () => console.log('mute'), icon: 'volume_up', location: 'left' },
-      { action: () => console.log('volume down'), icon: 'volume_down', location: 'left' },
-      { action: () => console.log('bookmark'), icon: 'bookmark', location: 'right' },
-      { action: () => console.log('minumize'), icon: 'fullscreen_exit', location: 'right' },
-      { action: () => console.log('fullscreen'), icon: 'fullscreen', location: 'right' },
+      { action: () => this.play(), icon: 'play_arrow', location: 'left' },
+      { action: () => this.stop(), icon: 'stop', location: 'left' },
+      { action: () => this.pause(), icon: 'pause', location: 'left' },
+      { action: () => this.previous(), icon: 'skip_previous', location: 'left' },
+      { action: () => this.next(), icon: 'skip_next', location: 'left' },
+      { action: () => this.mute(), icon: 'volume_mute', location: 'left' },
+      { action: () => this.volumeDown(), icon: 'volume_down', location: 'left' },
+      { action: () => this.volumeUp(), icon: 'volume_up', location: 'left' },
+      { action: () => this.bookmark(), icon: 'bookmark', location: 'right' },
+      { action: () => this.closeFullScreen(), icon: 'fullscreen_exit', location: 'right' },
+      { action: () => this.fullScreen(), icon: 'fullscreen', location: 'right' },
     ]
   };
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    document.addEventListener('fullscreenchange', (event) => {
+      if (document.fullscreenElement) {
+        this.isFullScreen = true;
+      } else {
+        this.isFullScreen = false;
+      }
+    });
+
+
+    this.videoElement.nativeElement.volume = this.volumeValue / 100;
+
   }
+
+  ngOnDestroy(): void {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+  public play(): void {
+    this.videoElement.nativeElement.play();
+  }
+  public stop(): void {
+    this.pause();
+    this.setCurrentTime(0);
+  }
+
+  public pause(): void {
+    this.videoElement.nativeElement.pause();
+  }
+
+  public previous(): void {
+    console.log('Previous video!');
+  }
+
+  public next(): void {
+    console.log('Next video');
+  }
+
+  public volumeUp(): void {
+    this.showTheVolumeElement();
+    if (this.videoElement.nativeElement.volume <= 0.90) {
+      this.videoElement.nativeElement.volume += 0.10;
+    }
+    this.updateVolumeValueFromVideoVolume();
+  }
+
+  public volumeDown(): void {
+    this.showTheVolumeElement();
+
+    if (this.videoElement.nativeElement.volume >= 0.10) {
+      this.videoElement.nativeElement.volume -= 0.10;
+    }
+    this.updateVolumeValueFromVideoVolume();
+  }
+
+  private showTheVolumeElement(): void {
+    if (this.isVolumeHidden) {
+      this.isVolumeHidden = false;
+      setTimeout(() => {
+        this.isVolumeHidden = true;
+      }, 3000);
+    }
+  }
+
+  public updateVolumeValue(): void {
+    this.videoElement.nativeElement.volume = this.volumeValue / 100;
+  }
+
+  private updateVolumeValueFromVideoVolume(): void {
+    this.volumeValue = this.videoElement.nativeElement.volume * 100;
+  }
+
+
+  public mute(): void {
+    this.showTheVolumeElement();
+    this.videoElement.nativeElement.volume = 0;
+    this.updateVolumeValueFromVideoVolume();
+  }
+
+
+  public bookmark(): void {
+    console.log('bookmarking');
+  }
+
+  private fullScreen(): void {
+    if (this.documentElement.requestFullscreen) {
+      this.documentElement.requestFullscreen();
+    }
+  }
+
+  private closeFullScreen(): void {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+
+  public setCurrentTime(time: number): void {
+    this.videoElement.nativeElement.currentTime = time;
+  }
+
+  public goToTime(event): void {
+    this.progressBarValue = 100;
+  }
+
+
 
 }
--- projects/ae-material/src/lib/ae-video/ae-video.module.ts
+++ projects/ae-material/src/lib/ae-video/ae-video.module.ts
@@ -4,16 +4,22 @@ import { AeVideoComponent } from './ae-video.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { AeToolbarModule } from '../ae-toolbar/ae-toolbar.module';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [AeVideoComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
     AeToolbarModule,
+    MatProgressBarModule,
+    MatSliderModule
   ],
   exports: [
     AeVideoComponent
